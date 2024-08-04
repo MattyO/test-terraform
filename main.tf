e00e973bb0d20a2aca78374568017663ba7152aa
Matt ODonnell <matt.odonnell@leandog.com>
2024-08-04 16:51:00 -0400

    stash

--- main.tf
+++ main.tf
@@ -5,4 +5,19 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           =  "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "test-instance"
+
+  }
+}
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.example.public_ip
 }

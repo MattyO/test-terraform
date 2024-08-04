772254745395aa747a0f90f274e3d737ac58e969
Matt ODonnell <matt.odonnell@leandog.com>
2024-08-04 17:15:15 -0400

    import

--- main.tf
+++ main.tf
@@ -2,6 +2,23 @@ provider "aws" {
     region = "us-east-2"
 }
 
+import {
+
+
+  id = "i-0d69eec292811ae91"
+  to = aws_instance.import-example
+}
+
+resource "aws_instance" "import-example" {
+  ami           =  "ami-0862be96e41dcbf74"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "test-import"
+
+  }
+}
+
 resource "aws_instance" "example" {
   ami           =  "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
